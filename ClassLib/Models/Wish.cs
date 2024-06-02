b3036895f4fa9f01f5081a4c685828e7c166be51
Josep Sendra <je.sendra@outlook.es>
2024-06-02 10:01:52 +0200

    Initial project

---
+++ ClassLib/Models/Wish.cs
@@ -0,0 +1,9 @@
+namespace GenieToolbox
+{
+    public class Wish
+    {
+        public GUID Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
